c33a9469ec7f9e1300fd0bdaed4296b5f039d8ea
Nithish Gowda <nithish.gowda@signify.com>
2023-08-14 20:54:55 +0530

    moved error line codes to handleError Function
    - fuction is encapsulated
    - can be used in same package only

--- randompsswd.go
+++ randompsswd.go
@@ -16,14 +16,10 @@ const (
 // function naming in Cap's can be used in other packages
 // can return two variables
 func GenerateRandomPassword(length int) (string, error) {
-
-	// If no invalid was given, return an error with a message.
-	if length <= 0 {
-		return "", errors.New("Invalid password length")
-	}
-
-	if length > 15 {
-		return "", errors.New("password length exceeds maximum")
+	// calling handleError function
+	err := handleError(length)
+	if err != nil {
+		return "", err
 	}
 
 	// seed the random number genarator with current time nanoseconds
@@ -46,3 +42,42 @@ func GenerateRandomPassword(length int) (string, error) {
 	// converting byte to string
 	return string(password), nil
 }
+
+// encpsulated function hidden from imported package
+func handleError(length int) error {
+	// If no invalid was given, return an error with a message.
+	if length <= 0 {
+		return errors.New("Invalid password length")
+	}
+
+	if length > 15 {
+		return errors.New("password length exceeds maximum")
+	}
+
+	return nil
+}
+
+// testing, change package name to main before testing
+/*
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("passwordGenarator: ")
+	log.SetFlags(0)
+
+	// Request a GenerateRandomPassword.
+	// If need to edit imported package file will be present in C:\Users\name\go\pkg\mod\github.com\nithishgwd\folder_name\file.go
+	password, err := GenerateRandomPassword(19)
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Printf("Password Generated -->>   %s", password)
+}
+*/
